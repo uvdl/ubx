8b1d0c78108dd4f7fd7d0a3ba6eb8922aaf776ef
John Kua <john.kua@mapper.ai>
2018-04-25 00:52:21 +0000

    Added NAV-PVT to capture message set

--- configure.sh
+++ configure.sh
@@ -10,7 +10,9 @@ DEVICE=/dev/ttyHSL2
 
 # Enable desired messages
 ./getset-messagerate.py --device $DEVICE --name ESF-STATUS --setRate 1
+# ./getset-messagerate.py --device $DEVICE --name ESF-RAW --setRate 1
 ./getset-messagerate.py --device $DEVICE --name HNR-PVT --setRate 1
+./getset-messagerate.py --device $DEVICE --name NAV-PVT --setRate 1
 ./getset-messagerate.py --device $DEVICE --name NAV-ATT --setRate 1
 ./getset-messagerate.py --device $DEVICE --name NAV-DOP --setRate 1
 ./getset-messagerate.py --device $DEVICE --name NAV-STATUS --setRate 1
