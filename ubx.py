4e6cf821e9e3173cd8a2f87efa27a01d97329b0c
John Kua <john.kua@mapper.ai>
2018-04-25 19:16:30 -0700

    Added command line options to capture and configure scripts for device specification and baud rate

--- ubx.py
+++ ubx.py
@@ -441,7 +441,7 @@ class Parser():
             self.fd = os.open(device, os.O_NONBLOCK | os.O_RDWR)
             try:
                 buf = os.read(self.fd, 512) # flush input
-                logging.debug("flushed %s" % repr(buf))
+                print("flushed %s" % repr(buf))
             except:
                 pass
             gobject.io_add_watch(self.fd, gobject.IO_IN, self.cbDeviceReadable)
